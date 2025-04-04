c4e8455f97eb0e8fc64792dedd5f35974e7deb12
Marco Manenti <marco.manenti@gmail.com>
2025-04-04 16:44:06 +0200

    Update eventSendMessageToTelegram.js

--- eventSendMessageToTelegram.js
+++ eventSendMessageToTelegram.js
@@ -32,18 +32,48 @@ let sendMessageToTelegram = function(message) {
     ExternalProcess.execute("curl", args);
 }
 
-// Esempio di utilizzo dello script
+// da attivare per debug messaggi inviati da Pixinsight allo script tramite env
+//sendMessageToTelegram("ENV DEBUG:\n" + JSON.stringify(env, null, 2));
+
+// chiamata per l'avvio dei singoli processi avviati da WBPP  (grazie per le integrazioni JS, Roberto Sartori!)
 if (env.event == "start") {
     sendMessageToTelegram(env.name);
-    if (env.group) {
-        sendMessageToTelegram("Processing " + env.group.frames.length + " frames");
+    if (env.group ) {
+        // env.group.activeFrames() available from WBPP 2.8.3
+        let totalFramesCount= env.group.fileItems.length;
+        let activeFramesCount = env.group.activeFrames().length;
+
+        sendMessageToTelegram(env.name + ": processing " + activeFramesCount + "/" + totalFramesCount + " active frames");
+
+
+        // da attivare in una futura release di WBPP
+        // Calcola la percentuale completata
+        //let percentage = Math.round((activeFramesCount / totalFramesCount) * 100);
+
+        // Calcola la barra di avanzamento
+        //let barLength = 20; // Lunghezza della barra
+        //let completed = Math.round((percentage / 100) * barLength);
+        //let progressBar = "[" + "#".repeat(completed) + ".".repeat(barLength - completed) + "]";
+        //sendMessageToTelegram(env.name + ": " + progressBar);
     }
 }
 
 if (env.event == "done") {
     if (env.status == OperationBlockStatus.DONE) {
         sendMessageToTelegram(env.name + " successfully executed");
-    } else if (env.status == OperationBlockStatus.FAILED) {
+    }
+    else if (env.status == OperationBlockStatus.FAILED) {
         sendMessageToTelegram(env.name + " failed, " + env.statusMessage);
     }
+}
+
+if (env.event == "pipeline start") {
+    sendMessageToTelegram(".-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-");
+    sendMessageToTelegram("WBPP started.");
+}
+
+if (env.event == "pipeline end") {
+    sendMessageToTelegram("WBPP terminated.");
+    sendMessageToTelegram(".-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-");
+
 }
